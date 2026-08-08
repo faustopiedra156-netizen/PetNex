62c9130e848beba8fcd5e8ab3ce627cf63131c9b
Fausto Piedra <faustopiedra156@gmail.com>
2026-08-07 21:13:08 -0500

    Mejora login y configuracion de hosts

--- petcare_loja/settings.py
+++ petcare_loja/settings.py
@@ -11,7 +11,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-petcare-loja-secret-k
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 CSRF_TRUSTED_ORIGINS = [
     origin.strip()
     for origin in os.environ.get(
